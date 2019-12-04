4e439d78620539325b1d4194bf3bbda2e335b579
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-04 21:59:35 +0200

    adding create error

--- routes/authors.js
+++ routes/authors.js
@@ -8,8 +8,20 @@ router.get('/',(req,res)=>{``
 router.get('/new',(req,res)=>{
     res.render('authors/new', {author:new Author()})
 });
-router.post('/',(req,res)=>{
-    res.send(req.body.name)
+
+router.post('/', async (req,res)=>{
+    const author = new Author({
+        name:req.body.name
+    });
+    try {
+        const newAuthor = await author.save();
+        res.redirect('authors')
+    } catch {
+        res.render('authors/new',{
+            author:author,
+            errorMessage : 'Error creating author'
+        })
+    }
 });
 
 module.exports = router
