70ee72389020bba508a6c13e5715d3a49fa64649
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-04 20:30:34 +0200

    adding input and req body name

--- routes/authors.js
+++ routes/authors.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Author = require('../models/author');
 
 router.get('/',(req,res)=>{``
     res.render('authors/index')
 });
 router.get('/new',(req,res)=>{
-    res.render('authors/new')
+    res.render('authors/new', {author:new Author()})
 });
 router.post('/',(req,res)=>{
-    res.send('Create')
+    res.send(req.body.name)
 });
 
 module.exports = router
