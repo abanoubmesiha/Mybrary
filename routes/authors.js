3682d31327c7f828a1411872c05acf5f9062e428
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-04 23:06:59 +0200

    adding search

--- routes/authors.js
+++ routes/authors.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Author = require('../models/author');
 
-router.get('/',(req,res)=>{``
-    res.render('authors/index')
+router.get('/', async (req,res)=>{
+    let searchOptions = {}
+    if (req.query.name != null || req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name,'i')
+    }
+    try {
+    const authors = await Author.find(searchOptions);
+    res.render('authors/index', {authors: authors,searchOptions:req.query})
+    } catch {
+        res.redirect('/')
+    }
 });
 router.get('/new',(req,res)=>{
     res.render('authors/new', {author:new Author()})
