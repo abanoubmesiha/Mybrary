29004a9fe203cf9b6d1ca1e42858a3db234c2d3e
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-04 12:55:33 +0200

    index route

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes');
 
 app.set('view engine','ejs')
 app.set('view',__dirname + '/views')
 app.set('layout', 'layout/layout');
 app.use(expressLayouts)
 app.use(express.static('public'));
+app.use('/',indexRouter);
+
 
 app.listen(process.env.PORT || 3000)
 
