d742fa2dc8a94a9dfbf7e49fca6e8494a516f7de
abanoubmesiha <53008015+abanoubmesiha@users.noreply.github.com>
2019-12-05 18:19:32 +0200

    finished file upload handling

--- routes/books.js
+++ routes/books.js
@@ -1,27 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 const Book = require('../models/book');
+const uploadPath = path.join('public', Book.coverImageBasePath)
 const Author = require('../models/author');
+const imageMimeTypes = ['image/jpeg','image/png','image/gif',]
+const upload = multer({
+    dest:uploadPath,
+    fileFilter:(req,file,callback) => {
+        callback(null,imageMimeTypes.includes(file.mimetype))
+    }
+})
 
 router.get('/', async (req,res)=>{
     res.send('all books')
 });
 router.get('/new',async (req,res)=>{
+    renderNewPage(res,new Book())
+});
+
+router.post('/',upload.single('cover'), async (req,res)=>{
+    const fileName = req.file != null ? req.file.filename : null
+    const book = new Book({
+        title:req.body.title,
+        description:req.body.description,
+        author:req.body.author,
+        coverImageName:fileName,
+        pageCount:req.body.pageCount,
+        publishDate:new Date(req.body.publishDate),
+    })
+    try {
+        const newBook = await book.save()
+        res.redirect(`books`)
+    } catch {
+        if (Book.coverImageName != null){
+            removeBookCover(Book.coverImageName)
+        }
+        renderNewPage(res,book,true)
+    }
+});
+function removeBookCover(fileName){
+    fs.unlink(path.join(uploadPath,fileName),err=>{
+        if (err) console.error(err)
+    })
+}
+async function renderNewPage(res, book, hasError = false){
     try {
         const authors = await Author.find({})
-        const book = new Book()
-        res.render('books/new',{
+        const params = {
             authors:authors,
             book:book
-        })
+        }
+        if (hasError) params.errorMessage = 'Error Creating Book'
+        res.render('books/new',params)
     } catch {
         res.send('error catched')
      //   res.redirect('/')
     }
-});
-
-router.post('/', async (req,res)=>{
-    res.send('create book')
-});
+}
 
 module.exports = router
